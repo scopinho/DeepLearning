b704e322f3a6683684fbb8530703f27f86735698
scopinho <eric.scopinho@gmail.com>
2023-03-07 18:51:00 -0300

    updated chdir

--- Fastai_bkp/L2-Production.py
+++ Fastai_bkp/L2-Production.py
@@ -3,12 +3,16 @@
 from fastai.vision.all import *
 #from time import sleep
 import gradio as gr
+import os
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
 
 def guess_bear(x): return x[0].isupper()
 
 learn = load_learner('class_bears.pkl')
 
-categories = ('black', 'grizzly', 'teddy')
+categories = ['black', 'grizzly', 'teddy']
 
 def classify_image(img):
     pred, idx, probs = learn.predict(img)
@@ -16,7 +20,7 @@ def classify_image(img):
 
 image = gr.inputs.Image(shape=(192,192))
 label = gr.outputs.Label()
-examples = ('black.jpg', 'grizzly.jpg', 'teddy.jpg')
+examples = ['bear_black.jpg', 'bear_grizzly.jpg', 'bear_teddy.jpg']
 
 intf = gr.Interface(fn=classify_image, inputs=image, outputs=label, examples=examples)
 intf.launch(inline=False)
